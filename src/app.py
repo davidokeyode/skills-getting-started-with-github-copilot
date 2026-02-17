734395c829becc81fa51436d9879a24005f10f02
David Okeyode <asegunlolu@hotmail.com>
2026-02-17 20:01:09 +0000

    Add comment to clarify initialization of activities in the API

--- src/app.py
+++ src/app.py
@@ -20,6 +20,7 @@ app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
           "static")), name="static")
 
 # In-memory activity database
+# Initialize activities with various categories
 activities = {
     "Chess Club": {
         "description": "Learn strategies and compete in chess tournaments",
